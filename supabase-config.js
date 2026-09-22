21ede4d3b1d3efbaf5d091681c2d303e97ddb626
dott-dopamin <szze.2i@gmail.com>
2026-09-22 12:30:23 +0900

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+window.JEI_SUPABASE_URL='https://yqvjgalmjrydfygtozgo.supabase.co';
+window.JEI_SUPABASE_KEY='sb_publishable_8K_sITH95Nkikw8-tdyDwQ_-JFzo94Z';
+window.jeiSupabase=window.supabase.createClient(window.JEI_SUPABASE_URL,window.JEI_SUPABASE_KEY);
